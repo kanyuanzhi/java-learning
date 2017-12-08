1961daf8fc10eee12d21c6ef787d0f372a57f989
kanyuanzhi <kan@mail.ustc.edu.cn>
2017-12-09 00:28:33 +0800

    change test

--- src/LoginDemo.java
+++ src/LoginDemo.java
@@ -28,7 +28,7 @@ class Input{
 }
 
 class Check {
-    private String username = "kanyuanzhi";
+    private String username = "kan";
     private String password = "123";
 
     public boolean validate(String username, String password) {
@@ -44,13 +44,13 @@ class Operate{
     private String info[];
 
     public Operate(String info[]){
-        this.info = info;
+        this.setInfo(info);
     }
 
     public String login(){
         Check check = new Check();
-        String username = this.info[0];
-        String password = this.info[1];
+        String username = this.getInfo()[0];
+        String password = this.getInfo()[1];
         String str = null;
         if (check.validate(username, password)){
             str = "welcome "+username;
@@ -58,5 +58,12 @@ class Operate{
             str = "WRONG!";
         }
         return str;
+    }
+
+    private void setInfo(String info[]){
+        this.info = info;
+    }
+    private String[] getInfo(){
+        return this.info;
     }
 }
